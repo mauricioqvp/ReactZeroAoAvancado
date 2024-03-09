8e257fd78b01c9a9c5f1d2a14ac1b6e9c3861c94
Maurício Quaranta <mauricioqvp@yahoo.com.br>
2024-03-09 17:23:05 -0300

    Componente chamando componente

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 
-const BomDia = (props) => {
-  return(
-    <h1>Bom dia, {props.name} !</h1>
+const Equipe = (props) => {
+  return (
+    <div>
+      <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade} fb={props.social} />
+    </div>
+  );
+}
+
+const Sobre = (props) => {
+  return (
+    <div>
+      <h2>Olá! Sou o {props.nome}</h2>
+      <h3>Cargo: {props.cargo} </h3>
+      <h3>Idade: {props.idade} </h3>
+      <Social fb={props.fb} />
+      <hr/>
+    </div>
+  );
+}
+
+const Social = (props) => {
+  return (
+    <div>
+      <a href={props.fb}>Facebook</a><br/>
+      <a href={props.fb}>LinkedIn</a>
+    </div>
   );
 }
 
 function App() {
   return (
     <div>
-      <BomDia name="Maurício" />
-      <h2>Curso React</h2>
-      <p>Este é um teste de React.</p>
-      Teste de escrita.
+      <h1>Conheça nossa equipe</h1>
+      <Equipe nome="Matheus" cargo="Programador" idade="29" social="https://google.com" />
     </div>
   );
 }
