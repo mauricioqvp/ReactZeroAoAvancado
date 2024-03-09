4873e1109ff8a3fef795bbb59d61c6896389faf0
Maurício Quaranta <mauricioqvp@yahoo.com.br>
2024-03-09 16:53:45 -0300

    Componente simples com props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 
+const BomDia = (props) => {
+  return(
+    <h1>Bom dia, {props.name} !</h1>
+  );
+}
+
 function App() {
   return (
     <div>
-      <h1>Olá mundo!</h1>
+      <BomDia name="Maurício" />
+      <h2>Curso React</h2>
+      <p>Este é um teste de React.</p>
+      Teste de escrita.
     </div>
   );
 }
