cce52202c1c25b5659e5c873c87f8e62bc05e6c2
Maurício Quaranta <mauricioqvp@yahoo.com.br>
2024-03-10 20:31:50 -0300

    Formularios 01

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
 import React, { Component } from "react";
-import Listagem from './components/Listagem'
 
 class App extends Component{
 
   constructor(props){
     super(props);
     this.state = {
-      feed:[
-        {id: 1, username: 'Matheus', curtidas: 10, comentarios: 2},
-        {id: 2, username: 'Lucas', curtidas: 63, comentarios: 21},
-        {id: 3, username: 'Amanda', curtidas: 120, comentarios: 12},
-        {id: 4, username: 'Amanda', curtidas: 1, comentarios: 0}
-      ]
+        email: 'teste@teste.com', 
+        senha: '123',
+        sexo: 'Masculino'
     }
+    this.atualizaEmail = this.atualizaEmail.bind(this);
+    this.atualizaSexo = this.atualizaSexo.bind(this);
+  }
+
+  atualizaEmail(e){
+    let novoEmail = e.target.value;
+    this.setState({email: novoEmail});
+  }
+
+  atualizaSexo(e){
+    let novoSexo = e.target.value;
+    this.setState({sexo: novoSexo});
   }
 
   render(){
     return(
-      <div>
-        { this.state.feed.map((item) => {
-          return(
-            <>
-              <Listagem cod={item.id} user={item.username} curtidas={item.curtidas} />
-            </>
-          )
-        })}
-      </div>
+      <>
+        E-mail:
+        <input type="text" name="email" value={this.state.email} 
+          onChange={this.atualizaEmail} /> <br/>
+        Senha:
+        <input type="text" name="senha" value={this.state.senha}
+          onChange={(e) => {this.setState({senha: e.target.value})}} /> <br/>
+        <select name="sexo" value={this.state.sexo} onChange={this.atualizaSexo}>
+          <option value="masculino">Masculino</option>
+          <option value="feminino">Feminino</option>
+        </select>
+
+        <div>
+          <h3>{this.state.email}</h3>
+          <h3>{this.state.senha}</h3>
+          <h3>{this.state.sexo}</h3>
+        </div>
+      </>
     );
   }
 }
