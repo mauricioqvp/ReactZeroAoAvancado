e9162afb71ccf23ab96a68ebcc831bd3d648dabd
Maurício Quaranta <mauricioqvp@yahoo.com.br>
2024-03-10 08:46:07 -0300

    Condicional, meu

--- src/App.js
+++ src/App.js
@@ -5,24 +5,41 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      status: false
-    };
-}
-
-    render(){
-      return(
-        <div>
-          {this.state.status ? 
-            <h2>Logado no sistema!</h2>
-            :
-            <h2>Por favor, faça seu Login</h2>
-          }
-          <div>
-            <h2>Bem vindo</h2>
-          </div>
-        </div>
-      );
+      logado: false,
+      usuario: 'Mauricio'
     }
+
+    this.entrar = this.entrar.bind(this);
+    this.sair = this.sair.bind(this);
+  }
+
+  entrar(){
+    console.log('Entrou no sistema!');
+    this.setState({logado: true});
+  }
+
+  sair(){
+    console.log('Saiu do sistema!');
+    this.setState({logado: false});
+  }
+
+  render(){
+    return(
+      <div>
+        {this.state.logado ?
+        <div>
+          <h2>Seja bem vindo ao sistema sr. {this.state.usuario}</h2>
+          <button onClick={this.sair}>Sair</button>
+        </div>
+          :
+        <div>
+          <h2>Você não está Logado</h2>
+          <button onClick={this.entrar}>Entrar</button>
+        </div>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
